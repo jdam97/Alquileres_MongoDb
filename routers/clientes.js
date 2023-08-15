76ecf7010ac0aeefb92ada64eed16c2515d88032
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-08-15 09:06:30 -0500

    refactor: arreglo el manejo de errores del router clientes

--- routers/clientes.js
+++ routers/clientes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { connectDB } from "../db/atlas.js";
 import { limitRequest } from "../config/limit.js";
-
 const Cliente = Router();
+
 let db = await connectDB();
+
 
 Cliente.use(limitRequest());
 
@@ -11,14 +12,16 @@ Cliente.use(limitRequest());
 
 //GET: traerme a todos los clientes
 Cliente.get("/", async (req,res) =>{
+    console.log(req.rateLimit);
     try {
         const collection = db.collection("cliente");
-        const data = await collection.find().toArray(); // .find es para traerme todos los datos de una tabla
+        const data = await collection.find({}).toArray(); // .find es para traerme todos los datos de una tabla
         res.send(data);
     } catch (error){
+        // console.log(error);
         res.status(500).json({
             message: "Error al listar los clientes", 
-            error
+            error:error.message,
         });
     }
 });
